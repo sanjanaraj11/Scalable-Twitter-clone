eaeaa7047822245d28340fb05e16144f0cf7211e
sanjanaraj11 <32622982+sanjanaraj11@users.noreply.github.com>
2018-10-16 15:06:17 -0700

    Create population.sql

---
+++ population.sql
@@ -0,0 +1,5 @@
+insert into user values (1, 'PrabhBhullar06','pbhullar0613@gmail.com','pbkdf2:sha256:50000$pL7KMUd1$85c0876396b4ecff34c9df1f91fc9b9169afe670fa3d60395dea99cae2b8b79f');
+insert into user values (2, 'ManvirSandhu13','manvir146@gmail.com','pbkdf2:sha256:50000$pL7KMUd1$85c0876396b4ecff34c9df1f91fc9b9169afe670fa3d60395dea99cae2b8b79f');
+insert into follower values (1,1); 
+insert into message values (1, 1,'How is your day going so far?', 24);
+insert into message values (2, 2,'Coding is fun', 25);
